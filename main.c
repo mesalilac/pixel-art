b9f725277f95525216652e72c7a0a97a9b90e917
Abdalrzag Eisa <bmjfdrh@gmail.com>
2024-04-24 06:22:29 +0300

    add a brush

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WIDTH            800
 #define HEIGHT           800
@@ -11,13 +12,17 @@
 #define GRID_MIN_HEIGHT 0
 #define GRID_MAX_WIDTH  WIDTH
 #define GRID_MAX_HEIGHT HEIGHT
+#define GRID_COLOR      32, 32, 32, 255
 
-#define CELL_SIZE  20
-#define CELL_COLOR 32, 32, 32, 255
+#define CELL_SIZE 20
 
 #define MAX_CELLS   (GRID_MAX_WIDTH / CELL_SIZE) * (GRID_MAX_HEIGHT / CELL_SIZE)
 #define MAX_ROWS    GRID_MAX_HEIGHT / CELL_SIZE - 1
 #define MAX_COLUMNS GRID_MAX_WIDTH / CELL_SIZE - 1
+
+#define ADD_COLOR(r, g, b)                                                     \
+    brush_colors.colors[brush_colors.size] = (SDL_Color){r, g, b, 255};        \
+    brush_colors.size++;
 
 typedef struct
 {
@@ -44,13 +49,27 @@ typedef struct
 typedef struct
 {
     GridPos grid_pos;
-} Player;
+} Brush;
 
-bool point_exists(Cell **points, int *points_size, Cell *cell)
+typedef struct
+{
+    SDL_Color colors[99];
+    int size;
+    int selected;
+} BrushColors;
+
+typedef struct
+{
+    int x;
+    int y;
+    SDL_Color color;
+} Point;
+
+bool point_exists(Point **points, int *points_size, Point *point)
 {
     for (int i = 0; i < *points_size; ++i)
     {
-        if (points[i]->x == cell->x && points[i]->y == cell->y)
+        if (points[i]->x == point->x && points[i]->y == point->y)
         {
             return true;
         }
@@ -59,18 +78,28 @@ bool point_exists(Cell **points, int *points_size, Cell *cell)
     return false;
 }
 
-void save_point(Cell **points, int *points_size, int x, int y)
+void save_point(
+    Point **points, int *points_size, BrushColors *brush_colors, int x, int y
+)
 {
-    Cell *cell = malloc(sizeof(Cell));
-    cell->x    = x;
-    cell->y    = y;
+    Point *point = malloc(sizeof(Point));
+    point->x     = x;
+    point->y     = y;
+    point->color = brush_colors->colors[brush_colors->selected];
 
-    if (point_exists(points, points_size, cell) == false)
+    // TODO if point exists change color
+    for (int i = 0; i < *points_size; ++i)
     {
-        points[*points_size] = cell;
-        *points_size += 1;
-        printf("x: %i, y: %i\n", x, y);
+        if (points[i]->x == point->x && points[i]->y == point->y)
+        {
+            points[i]->color = point->color;
+            return;
+        }
     }
+
+    points[*points_size] = point;
+    *points_size += 1;
+    printf("x: %i, y: %i, color_selected: %i\n", x, y, brush_colors->selected);
 }
 
 void build_grid(Cells *cells)
@@ -98,7 +127,7 @@ void draw_grid(SDL_Renderer *ren, Cells *cells)
             SDL_Rect rect = {
                 .x = cell.x, .y = cell.y, .w = CELL_SIZE, .h = CELL_SIZE};
 
-            SDL_SetRenderDrawColor(ren, CELL_COLOR);
+            SDL_SetRenderDrawColor(ren, GRID_COLOR);
             SDL_RenderDrawRect(ren, &rect);
         }
     }
@@ -106,6 +135,8 @@ void draw_grid(SDL_Renderer *ren, Cells *cells)
 
 int main()
 {
+    srand(time(0));
+
     if (SDL_Init(SDL_INIT_VIDEO) == -1)
     {
         fprintf(
@@ -136,12 +167,44 @@ int main()
     Cells cells = {.size = 0};
     build_grid(&cells);
 
-    Cell **points   = malloc(MAX_CELLS * sizeof(Cell));
+    Point **points  = malloc(MAX_CELLS * sizeof(Point));
     int points_size = 0;
 
-    Player player = {
+    Brush brush = {
         .grid_pos = {.row = 0, .column = 0}
     };
+
+    BrushColors brush_colors = {.size = 0, .selected = 0};
+
+    ADD_COLOR(255, 255, 255)
+    ADD_COLOR(101, 101, 101)
+    ADD_COLOR(20, 20, 20)
+    ADD_COLOR(243, 46, 50)
+    ADD_COLOR(243, 242, 46)
+    ADD_COLOR(105, 243, 46)
+    ADD_COLOR(46, 243, 101)
+    ADD_COLOR(46, 243, 234)
+    ADD_COLOR(46, 151, 243)
+    ADD_COLOR(46, 88, 243)
+    ADD_COLOR(52, 46, 243)
+    ADD_COLOR(77, 46, 243)
+    ADD_COLOR(134, 46, 243)
+    ADD_COLOR(193, 46, 243)
+    ADD_COLOR(243, 46, 226)
+    ADD_COLOR(243, 46, 145)
+
+    /*
+    for (int i = 0; i < cells.size.w; i++)
+    {
+        for (int j = 0; j < cells.size.h; j++)
+        {
+            Cell cell             = cells.grid[j][i];
+            brush_colors.selected = rand() % (brush_colors.size - 1 - 0);
+            save_point(points, &points_size, &brush_colors, cell.x, cell.y);
+        }
+    }
+    brush_colors.selected = 0;
+    */
 
     while (is_running)
     {
@@ -157,38 +220,37 @@ int main()
                         save_point(
                             points,
                             &points_size,
+                            &brush_colors,
                             cells
-                                .grid[player.grid_pos.row]
-                                     [player.grid_pos.column]
+                                .grid[brush.grid_pos.row][brush.grid_pos.column]
                                 .x,
 
                             cells
-                                .grid[player.grid_pos.row]
-                                     [player.grid_pos.column]
+                                .grid[brush.grid_pos.row][brush.grid_pos.column]
                                 .y
                         );
                     // Move up
                     if (event.key.keysym.sym == 'k')
                     {
-                        if (player.grid_pos.row == 0)
+                        if (brush.grid_pos.row == 0)
                         {
-                            player.grid_pos.row = MAX_ROWS;
+                            brush.grid_pos.row = MAX_ROWS;
                         }
                         else
                         {
-                            player.grid_pos.row--;
+                            brush.grid_pos.row--;
                         }
                     }
                     // Move down
                     if (event.key.keysym.sym == 'j')
                     {
-                        if (player.grid_pos.row == MAX_ROWS)
+                        if (brush.grid_pos.row == MAX_ROWS)
                         {
-                            player.grid_pos.row = 0;
+                            brush.grid_pos.row = 0;
                         }
                         else
                         {
-                            player.grid_pos.row++;
+                            brush.grid_pos.row++;
                         }
                     }
                     // Move left
@@ -196,25 +258,25 @@ int main()
                     //       going left or right skips one cell
                     if (event.key.keysym.sym == 'h')
                     {
-                        if (player.grid_pos.column == 0)
+                        if (brush.grid_pos.column == 0)
                         {
-                            player.grid_pos.column = MAX_COLUMNS;
+                            brush.grid_pos.column = MAX_COLUMNS;
                         }
                         else
                         {
-                            player.grid_pos.column--;
+                            brush.grid_pos.column--;
                         }
                     }
                     // Move right
                     if (event.key.keysym.sym == 'l')
                     {
-                        if (player.grid_pos.column == MAX_COLUMNS)
+                        if (brush.grid_pos.column == MAX_COLUMNS)
                         {
-                            player.grid_pos.column = 0;
+                            brush.grid_pos.column = 0;
                         }
                         else
                         {
-                            player.grid_pos.column++;
+                            brush.grid_pos.column++;
                         }
                     }
                     if (event.key.keysym.sym == 's')
@@ -222,20 +284,41 @@ int main()
                         save_point(
                             points,
                             &points_size,
+                            &brush_colors,
                             cells
-                                .grid[player.grid_pos.row]
-                                     [player.grid_pos.column]
+                                .grid[brush.grid_pos.row][brush.grid_pos.column]
                                 .x,
 
                             cells
-                                .grid[player.grid_pos.row]
-                                     [player.grid_pos.column]
+                                .grid[brush.grid_pos.row][brush.grid_pos.column]
                                 .y
                         );
                         saving = true;
                     }
                     if (event.key.keysym.sym == 'c')
                         points_size = 0;
+                    if (event.key.keysym.sym == 'p')
+                    {
+                        if (brush_colors.selected == 0)
+                        {
+                            brush_colors.selected = brush_colors.size - 1;
+                        }
+                        else
+                        {
+                            brush_colors.selected--;
+                        }
+                    }
+                    if (event.key.keysym.sym == 'n')
+                    {
+                        if (brush_colors.selected == brush_colors.size - 1)
+                        {
+                            brush_colors.selected = 0;
+                        }
+                        else
+                        {
+                            brush_colors.selected++;
+                        }
+                    }
                     break;
                 case SDL_KEYUP:
                     if (event.key.keysym.sym == 's')
@@ -243,10 +326,6 @@ int main()
                     break;
             }
         }
-
-        printf(
-            "player:r(%i)c(%i)\n", player.grid_pos.row, player.grid_pos.column
-        );
 
         int mouse_x, mouse_y;
         Uint32 buttons   = SDL_GetMouseState(&mouse_x, &mouse_y);
@@ -264,7 +343,9 @@ int main()
                 if (SDL_PointInRect(&cursor, &rect) &&
                     (buttons & SDL_BUTTON_LMASK) != 0)
                 {
-                    save_point(points, &points_size, cell.x, cell.y);
+                    save_point(
+                        points, &points_size, &brush_colors, cell.x, cell.y
+                    );
                 }
             }
         }
@@ -276,22 +357,35 @@ int main()
 
         for (int i = 0; i < points_size; ++i)
         {
-            Cell *point = points[i];
+            Point *point = points[i];
 
             SDL_Rect rect = {
                 .x = point->x, .y = point->y, .w = CELL_SIZE, .h = CELL_SIZE};
 
-            SDL_SetRenderDrawColor(ren, 200, 200, 0, 255);
+            SDL_SetRenderDrawColor(
+                ren,
+                point->color.r,
+                point->color.g,
+                point->color.b,
+                point->color.a
+            );
             SDL_RenderFillRect(ren, &rect);
         }
 
-        SDL_Rect player_rect = {
-            .x = cells.grid[player.grid_pos.row][player.grid_pos.column].x,
-            .y = cells.grid[player.grid_pos.row][player.grid_pos.column].y,
+        SDL_Rect brush_rect = {
+            .x = cells.grid[brush.grid_pos.row][brush.grid_pos.column].x,
+            .y = cells.grid[brush.grid_pos.row][brush.grid_pos.column].y,
             .w = CELL_SIZE,
             .h = CELL_SIZE};
-        SDL_SetRenderDrawColor(ren, 200, 0, 0, 255);
-        SDL_RenderFillRect(ren, &player_rect);
+
+        SDL_SetRenderDrawColor(
+            ren,
+            brush_colors.colors[brush_colors.selected].r,
+            brush_colors.colors[brush_colors.selected].g,
+            brush_colors.colors[brush_colors.selected].b,
+            brush_colors.colors[brush_colors.selected].a
+        );
+        SDL_RenderDrawRect(ren, &brush_rect);
 
         SDL_RenderPresent(ren);
     }
